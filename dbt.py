970a201df5302faf884411681fd647c3c894f6d6
Andraz Brodnik <brodul@brodul.org>
2022-12-06 13:49:52 +0100

    Add new synq specific DAG

--- dbt.py
+++ dbt.py
@@ -6,9 +6,10 @@ from airflow_dbt.operators.dbt_operator import (
     DbtRunOperator,
     DbtTestOperator,
 )
+from airflow.operators.python import ShortCircuitOperator
 from airflow.utils.dates import days_ago
 
-synq_token = Variable.get("synq_token")
+synq_token = Variable.get("synq_token", default_var=None)
 
 default_args = {
     "dir": "/opt/airflow/dags/repo/dbt_example",
@@ -16,8 +17,34 @@ default_args = {
     "profiles_dir": "/opt/airflow/dags/repo/dbt_example",
 }
 
+###
+# DAGs
+###
+
+# Vanilla dbt
 with DAG(dag_id="dbt", default_args=default_args, schedule_interval="@daily") as dag:
 
+    dbt_seed = DbtSeedOperator(task_id="dbt_seed")
+
+    dbt_snapshot = DbtSnapshotOperator(task_id="dbt_snapshot")
+
+    dbt_run = DbtRunOperator(task_id="dbt_run")
+
+    dbt_test = DbtTestOperator(
+        task_id="dbt_test",
+        retries=0,  # Failing tests would fail the task, and we don't want Airflow to try again
+    )
+
+    dbt_seed >> dbt_snapshot >> dbt_run >> dbt_test
+
+# Dbt reporting to synq
+with DAG(
+    dag_id="dbt_with_synq", default_args=default_args, schedule_interval="@daily"
+) as dag:
+    # We need the synq tooken for synq integrated dags
+    synq_token_defined = ShortCircuitOperator(
+        task_id="synq_token_defined", python_callable=lambda: synq_token
+    )
     dbt_seed = DbtSeedOperator(task_id="dbt_seed")
 
     dbt_snapshot = DbtSnapshotOperator(task_id="dbt_snapshot")
