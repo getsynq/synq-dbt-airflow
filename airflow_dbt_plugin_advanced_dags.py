e98d19759803ddb666c580f430fe95b920a80b18
Lukasz Stefaniak <lustefaniak@gmail.com>
2023-03-03 00:06:40 +0100

    Change Table usage

--- airflow_dbt_plugin_advanced_dags.py
+++ airflow_dbt_plugin_advanced_dags.py
@@ -94,7 +94,7 @@ with DAG(
     )
 
     copy_tables = DummyOperator(task_id="copy_tables", inlets=AUTO, outlets=[
-        Table("bigquery-db", "schema-maybe", "table-name"),
+        Table(database="bigquery-db", cluster="schema-maybe", name="table-name"),
         Urn(
             "dbt-sh-d28816dc-7bba-11ed-bdbb-c465160955e4::model.dbt_example.my_first_dbt_model"
         ),
